d1c4f9431c1e26c474b9fbd91483226b335c26d5
Armando Prieto <aprieto@cryoport.com>
2024-05-20 16:54:59 -0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,98 @@
+
+## Micronaut Jasypt Library
+
+This library provides easy-to-use integration with Jasypt for encrypting and decrypting properties in your Micronaut applications. Inspired by [jasypt-spring-boot](https://github.com/ulisesbocchio/jasypt-spring-boot), this library simplifies the setup and usage of Jasypt in Micronaut projects.
+
+### Features
+
+- Seamless integration with Micronaut configuration.
+- Encrypt and decrypt properties in application configuration files.
+
+### Getting Started
+
+#### 1. Add the Dependency
+
+To use the Micronaut Jasypt library, add the following dependency to your `build.gradle` file:
+
+```groovy
+dependencies {
+    implementation 'io.github.aprietop:jasypt-micronaut:1.0.0'
+}
+```
+
+#### 2. Configuration
+
+Configure the library in your `application.yml` or `application.properties` file. The essential configuration properties include the encryption password and the algorithm.
+
+##### application.yml
+
+```yaml
+jasypt:
+  encryption:
+    password: ${JASYPT_PASSWORD} #env. variabale
+```
+
+##### application.properties
+
+```properties
+# env. variabale
+jasypt.encryption.password=${JASYPT_PASSWORD} 
+```
+
+#### 3. Usage
+
+Encrypt your sensitive properties using Jasypt CLI or any other method. Encrypted properties should be prefixed with `ENC(` and suffixed with `)`.
+
+##### Example
+
+Encrypt a property value:
+
+```sh
+jasypt encrypt input="my-secret-value" password="your-encryption-password"
+```
+
+Output:
+
+```
+ENC(GO9jHf7yVuP4E7oGzQmYkQ==)
+```
+
+Use the encrypted value in your `application.yml` or `application.properties`:
+
+```yaml
+my:
+  secret:
+    property: ${ENC(GO9jHf7yVuP4E7oGzQmYkQ==)}
+```
+
+```properties
+my.secret.property=${ENC(GO9jHf7yVuP4E7oGzQmYkQ==)}
+```
+
+#### 4. Accessing Decrypted Properties
+
+You can inject and use the decrypted properties in your Micronaut beans just like any other configuration property:
+
+```java
+import io.micronaut.context.annotation.Value;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class MyService {
+
+    @Value("${my.secret.property}")
+    private String secretProperty;
+
+    public void printSecret() {
+        System.out.println("Decrypted property value: " + secretProperty);
+    }
+}
+```
+
+### Contributions
+
+Contributions are welcome! Please fork the repository and submit pull requests for any enhancements or bug fixes.
+
+### License
+
+This project is licensed under the Apache-2.0 License. See the [LICENSE](LICENSE) file for details.
---
+++ src/main/java/io/github/aprietop/JasyptPropertyExpressionResolver.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.aprietop;
+
+import io.micronaut.context.env.PropertyExpressionResolver;
+import io.micronaut.core.convert.ConversionService;
+import io.micronaut.core.value.PropertyResolver;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+import java.util.Optional;
+
+/**
+ * Jasypt property resolver for decrypting properties in application property files like {@code application.properties} or {@code application.yml} using Jasypt.
+ * <p>
+ * This resolver requires a Jasypt encryption password to be specified in the properties file with the default key {@code jasypt.encryption.password}.
+ * The password is used to decrypt any properties that are encrypted using Jasypt.
+ * <p>
+ * Example configuration in {@code application.yml}:
+ *
+ * <pre>
+ * jasypt:
+ *   encryption:
+ *     password: your-encryption-password
+ * </pre>
+ * <p>
+ * Example encrypted property:
+ *
+ * <pre>
+ * my:
+ *   secret:
+ *     property: ${ENC(GO9jHf7yVuP4E7oGzQmYkQ==)}
+ * </pre>
+ * <p>
+ * This class provides methods to check for the presence of encrypted properties and decrypt them as needed.
+ * <p>
+ * Usage example:
+ *
+ * <pre>
+ * {@code
+ * @Singleton
+ * public class MyService {
+ *
+ *     @Value("${my.secret.property}")
+ *     private String secretProperty;
+ *
+ *     public void printSecret() {
+ *         System.out.println("Decrypted property value: " + secretProperty);
+ *     }
+ * }
+ * }
+ * </pre>
+ *
+ * @author Armando Prieto
+ * @since 1.0.0
+ */
+public class JasyptPropertyExpressionResolver implements PropertyExpressionResolver {
+
+    private static final String JASYPT_PREFIX = "ENC(";
+    public static final String JASYPT_PASSWORD_PROPERTY_NAME = "jasypt.encryption.password";
+    private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+
+    @Override
+    public <T> Optional<T> resolve(PropertyResolver propertyResolver,
+                                   ConversionService conversionService,
+                                   String expression,
+                                   Class<T> requiredType) {
+        if (isJasyptExpression(expression)) {
+            if (!encryptor.isInitialized()) {
+                propertyResolver.getProperty(JASYPT_PASSWORD_PROPERTY_NAME, String.class)
+                        .ifPresent(encryptor::setPassword);
+            }
+            return Optional.of(conversionService.convertRequired(decrypt(expression), requiredType));
+        }
+        return Optional.empty();
+    }
+
+    boolean isJasyptExpression(String expression) {
+        return expression.startsWith(JASYPT_PREFIX);
+    }
+
+    String decrypt(String expression) {
+        return this.encryptor.decrypt(extractValue(expression));
+    }
+
+    String extractValue(String expression) {
+        return expression.substring(JASYPT_PREFIX.length(), expression.length() - 1);
+    }
+}
